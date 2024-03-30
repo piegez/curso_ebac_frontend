7759c316e6e2d19b7d49f3ac66f26822992e8209
Jessica Sampaio <jessicamsampaio@gmail.com>
2024-03-30 18:51:27 -0300

    atualizacao

--- oo_js.js
+++ oo_js.js
@@ -1,3 +1,4 @@
+// primeiro vou criar a classe com as instâncias
 class Professor {
     constructor(nome) {
         this.nome = nome;
@@ -20,31 +21,27 @@ class Professor {
     }
 }
 
-class ProfessorMatematica extends Professor {
+// determinando o que via ser considerado final
+class ProfessorFinal extends Professor {
     constructor(nome) {
         super(nome);
     }
 }
 
-class ProfessorHistoria extends Professor {
-    constructor(nome) {
-        super(nome);
-    }
-}
-
-const professorJoao = new ProfessorMatematica('João');
+//criando constantes 
+const professorJoao = new  ProfessorFinal('João');
 professorJoao.setAulas(5);
 professorJoao.setHorasTrabalhadas(4);
 
-const professorMaria = new ProfessorHistoria('Maria');
+const professorMaria = new  ProfessorFinal('Maria');
 professorMaria.setAulas(4);
 professorMaria.setHorasTrabalhadas(3);
 
-const professorPedro = new ProfessorMatematica('Pedro');
+const professorPedro = new  ProfessorFinal('Pedro');
 professorPedro.setAulas(6);
 professorPedro.setHorasTrabalhadas(5);
 
-// Exibindo resultados
+//chamando as constantes
 console.log(professorJoao.getInfo());
 console.log(professorMaria.getInfo());
 console.log(professorPedro.getInfo());
