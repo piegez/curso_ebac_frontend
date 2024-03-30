ac532727a1e9b3aa5b2ea511c90f510148136692
Jessica Sampaio <jessicamsampaio@gmail.com>
2024-03-30 18:48:41 -0300

    adição extends

--- oo_js.js
+++ oo_js.js
@@ -1,74 +1,50 @@
-    class Professor {
-        constructor(nome) {
+class Professor {
+    constructor(nome) {
         this.nome = nome;
-        }
-    
-        setAulas(quantidadeAulas) {
+    }
+
+    setAulas(quantidadeAulas) {
         this.quantidadeAulas = quantidadeAulas;
-        }
-    
-        setHorasTrabalhadas(horasPorAula) {
+    }
+
+    setHorasTrabalhadas(horasPorAula) {
         this.horasPorAula = horasPorAula;
-        }
-    
-        calcularHorasPorSemana() {
+    }
+
+    calcularHorasPorSemana() {
         return this.quantidadeAulas * this.horasPorAula;
-        }
-    
-        getInfo() {
+    }
+
+    getInfo() {
         return `O professor ${this.nome} dá ${this.quantidadeAulas} aulas e trabalha ${this.calcularHorasPorSemana()} horas por semana.`;
-        }
     }
-    
-    class ProfessorMatematica {
-        constructor(nome) {
-        this.professor = new Professor(nome);
-        }
-    
-        setAulas(quantidadeAulas) {
-        this.professor.setAulas(quantidadeAulas);
-        }
-    
-        setHorasTrabalhadas(horasPorAula) {
-        this.professor.setHorasTrabalhadas(horasPorAula);
-        }
-    
-        getInfo() {
-        return this.professor.getInfo();
-        }
+}
+
+class ProfessorMatematica extends Professor {
+    constructor(nome) {
+        super(nome);
     }
-    
-    class ProfessorHistoria {
-        constructor(nome) {
-        this.professor = new Professor(nome);
-        }
-    
-        setAulas(quantidadeAulas) {
-        this.professor.setAulas(quantidadeAulas);
-        }
-    
-        setHorasTrabalhadas(horasPorAula) {
-        this.professor.setHorasTrabalhadas(horasPorAula);
-        }
-    
-        getInfo() {
-        return this.professor.getInfo();
-        }
+}
+
+class ProfessorHistoria extends Professor {
+    constructor(nome) {
+        super(nome);
     }
-    
-    const professorJoao = new ProfessorMatematica('João');
-    professorJoao.setAulas(5);
-    professorJoao.setHorasTrabalhadas(4);
-    
-    const professorMaria = new ProfessorHistoria('Maria');
-    professorMaria.setAulas(4);
-    professorMaria.setHorasTrabalhadas(3);
-    
-    const professorPedro = new ProfessorMatematica('Pedro');
-    professorPedro.setAulas(6);
-    professorPedro.setHorasTrabalhadas(5);
-    
-    // Exibindo resultados
-    console.log(professorJoao.getInfo());
-    console.log(professorMaria.getInfo());
-    console.log(professorPedro.getInfo());
+}
+
+const professorJoao = new ProfessorMatematica('João');
+professorJoao.setAulas(5);
+professorJoao.setHorasTrabalhadas(4);
+
+const professorMaria = new ProfessorHistoria('Maria');
+professorMaria.setAulas(4);
+professorMaria.setHorasTrabalhadas(3);
+
+const professorPedro = new ProfessorMatematica('Pedro');
+professorPedro.setAulas(6);
+professorPedro.setHorasTrabalhadas(5);
+
+// Exibindo resultados
+console.log(professorJoao.getInfo());
+console.log(professorMaria.getInfo());
+console.log(professorPedro.getInfo());
